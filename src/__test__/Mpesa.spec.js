55b646ab72ce4374fedaff12c4f8a6cddb767a79
kwahalf <kwanusud@gmail.com>
2018-04-19 15:45:48 +0300

    [feature #156912089] add api functionality and test

--- src/__test__/Mpesa.spec.js
+++ src/__test__/Mpesa.spec.js
@@ -1,4 +1,17 @@
 import Mpesa from '../Mpesa';
+import axios, { //eslint-disable-line no-unused-vars
+  B2Cdata,
+  B2Bdata,
+  C2BRegisterdata,
+  C2BSimulatedata,
+  LipaNaMpesaOnlinedata,
+  LipaNaMpesaQuerydata,
+  Reversaldata,
+  TransactionStatusdata,
+  AccountBalancedata,
+} from '../../__mocks__/mockapi';
+
+jest.disableAutomock();
 
 const credetials = {
   customer_key: 'exampleCustomerKey',
@@ -15,5 +28,103 @@ describe('Mpesa', () => {
     expect(mpesa.customer_key).toBe(credetials.customer_key);
     expect(mpesa.customer_secret).toBe(credetials.customer_secret);
     expect(mpesa.securityCredential).toBe(credetials.securityCredential);
+  });
+});
+
+describe('security', () => {
+  const mpesa = new Mpesa(credetials, 'development');
+  const encrypt = mpesa.security();
+  it('should return correct security credential', () => {
+    expect(encrypt).toBeTruthy();
+  });
+});
+
+describe('B2C', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.B2C(B2Cdata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('B2B', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.B2B(B2Bdata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('C2BRegister', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.C2BRegister(C2BRegisterdata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('C2BSimulate', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.C2BSimulate(C2BSimulatedata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('LipaNaMpesaOnline', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.LipaNaMpesaOnline(LipaNaMpesaOnlinedata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('LipaNaMpesaQuery', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.LipaNaMpesaQuery(LipaNaMpesaQuerydata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('LipaNaMpesaQuery', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.Reversal(Reversaldata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('TransactionStatus', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.TransactionStatus(TransactionStatusdata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
+  });
+});
+
+describe('AccountBalance', () => {
+  it('should make a successful call', async () => {
+   const mpesa = new Mpesa(credetials, 'development');
+   const r = await mpesa.AccountBalance(AccountBalancedata);
+   expect(r.data.data).toBe("sucess");
+   expect(r.config.headers.Authorization).toBe("Bearer sampleAcessToken");
+   expect(r.config.headers.Accept).toContain("application/json");
   });
 });
--- src/routes.js
+++ src/routes.js
@@ -1,44 +1,18 @@
 const Routes = {
-    auth: {
-        production: "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
-        development: "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+    baseUrl: {
+        production: "https://api.safaricom.co.ke",
+        development: "https://sandbox.safaricom.co.ke",
     },
-    B2C: {
-        production: "https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest",
-        development: "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest",
-    },
-    B2B: {
-        production: "https://api.safaricom.co.ke/mpesa/b2b/v1/paymentrequest",
-        development: "https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest",
-    },
-    C2B_Register: {
-        production: "https://api.safaricom.co.ke/mpesa/c2b/v1/registerurl",
-        development: "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl",
-    },
-    C2B_Simulate: {
-        production: "https://api.safaricom.co.ke/mpesa/c2b/v1/simulate",
-        development: "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate",
-    },
-    Lipa_Na_Mpesa_Online: {
-        production: "https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-        development: "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-    },
-    Lipa_Na_Mpesa_Query: {
-        production: "https://api.safaricom.co.ke/mpesa/stkpushquery/v1/query",
-        development: "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query",
-    },
-    Reversal: {
-        production: "https://api.safaricom.co.ke/mpesa/reversal/v1/request",
-        development: "https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request"
-    },
-    Transaction_Status: {
-        production: "https://api.safaricom.co.ke/mpesa/transactionstatus/v1/query",
-        development: "https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query",
-    },
-    Account_Balance: {
-        production: "https://api.safaricom.co.ke/mpesa/accountbalance/v1/query",
-        development: "https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query",
-    },
+    auth: "/oauth/v1/generate?grant_type=client_credentials",
+    B2C: "/mpesa/b2c/v1/paymentrequest",
+    B2B: "/mpesa/b2b/v1/paymentrequest",
+    C2BRegister: "/mpesa/c2b/v1/registerurl",
+    C2BSimulate: "/mpesa/c2b/v1/simulate",
+    LipaNaMpesaOnline: "/mpesa/stkpush/v1/processrequest",
+    LipaNaMpesaQuery: "/mpesa/stkpushquery/v1/query",
+    Reversal: "/mpesa/reversal/v1/request",
+    TransactionStatus: "/mpesa/transactionstatus/v1/query",
+    AccountBalance: "/mpesa/accountbalance/v1/query",
 };
 
 export default Routes;
